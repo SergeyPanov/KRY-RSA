bcdc346d8590b31797841f026f51b2b41a0b83ee
SergeyPanov <panovsy@gmail.com>
2018-04-25 12:45:39 +0200

    rho factorization added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "/Users/sergeypanov/bin/gmp/include/gmp.h"
 #include <random>
+#define ITERATIONS 1000000
+#define K 100
 
 class mpz{
 public:
@@ -40,7 +42,10 @@ void set_random_odd(int bit_size, mpz_t r_number){
     gmp_randseed_ui(r_state, seed);
 
     mpz_urandomb(r_number, r_state, bit_size);
+
+    // Set 2 MSB as 1
     mpz_setbit(r_number, bit_size - 1);
+    mpz_setbit(r_number, bit_size - 2);
 
 
     mpz_t reminder;
@@ -68,51 +73,6 @@ void set_random_odd(int bit_size, mpz_t r_number){
     mpz_clear(two);
     mpz_clear(reminder);
     gmp_randclear(r_state);
-}
-
-// Check if number is prime
-bool check_is_prime(mpz_t number){
-    bool is_prime = true;
-
-    mpz_t divisor;
-    mpz_init(divisor);
-
-    mpz_set_ui(divisor, 2);
-
-    mpz_t quotient;
-    mpz_init(quotient);
-
-    mpz_div(quotient, number, divisor); // Div by 2
-
-    mpz_t inc;
-    mpz_init(inc);
-    mpz_set_ui(inc, 1);
-
-
-    mpz_t reminder;
-    mpz_init(reminder);
-
-
-
-    while (mpz_cmp(quotient, divisor) > 0){
-
-        mpz_mod(reminder, number, divisor);
-
-        if (mpz_sgn(reminder) == 0){
-            is_prime = false;
-            break;
-        }
-
-        mpz_add(divisor, divisor, inc);
-    }
-
-
-    mpz_clear(inc);
-    mpz_clear(divisor);
-    mpz_clear(reminder);
-    mpz_clear(quotient);
-
-    return is_prime;
 }
 
 
@@ -227,7 +187,7 @@ void set_inverse(mpz_t a, mpz_t n, mpz_t x) {
     mpz_clear(old_a);
 }
 
-
+// Set dst to random number [min .. max - 1)
 void set_random(int min, mpz_t max, mpz_t dst){
 
 
@@ -255,7 +215,7 @@ void set_random(int min, mpz_t max, mpz_t dst){
 
     gmp_randclear(state);
 }
-
+// Set r, d. See Miller-Rabin algorithm
 void set_r_d(mpz &number, mpz &r, mpz &d){
 
     mpz acc, reminder;
@@ -282,7 +242,7 @@ void set_r_d(mpz &number, mpz &r, mpz &d){
 
 }
 
-
+// Check if number is prime. Used Miller-Rabin algorithm
 bool miller_rabin_is_prime(mpz &number, int k){
     mpz r, d, right_bound, a;
 
@@ -341,17 +301,20 @@ void set_prime(int bit_size, mpz &number){
 
     set_random_odd(bit_size, number.value);
 
-    while (!miller_rabin_is_prime(number, 100)){
+    while (!miller_rabin_is_prime(number, K)){
         mpz_add_ui(number.value, number.value,2);
     }
 }
 // Calculate e
-void set_e(int min, mpz &e, mpz &phi){
-
-    mpz  r_n, gcd, init_gcd;
-    mpz_set(init_gcd.value, gcd.value);
+void set_e(int min, mpz &e, mpz &phi, int bit_size){
+    mpz  r_n, gcd;
 
     set_random(min, phi.value, e.value);
+
+    // Set 2 MSB as 1
+    mpz_setbit(e.value, bit_size - 1);
+    mpz_setbit(e.value, bit_size - 2);
+
 
     set_gcd(e.value, phi.value, gcd.value);
 
@@ -410,7 +373,6 @@ void RSA_prepare(mpz &p, mpz &q, mpz &n, mpz &e, mpz &d, int bit_size){
     mpz_mul(n.value, p.value, q.value); // Get n
     display("n: ", n.value);
 
-
     mpz p_dec, q_dec;
 
     mpz_sub_ui(p_dec.value, p.value, 1);
@@ -421,7 +383,7 @@ void RSA_prepare(mpz &p, mpz &q, mpz &n, mpz &e, mpz &d, int bit_size){
 
     display("phi: ", phi.value);
 
-    set_e(1, e, phi);   // Get e
+    set_e(1, e, phi, bit_size);   // Get e
 
     display("e: ", e.value);
 
@@ -440,28 +402,154 @@ void decipher(mpz &d, mpz &n, mpz &c, mpz &m){
     mpz_powm(m.value, c.value, d.value, n.value);
 }
 
+// True if successfully False otherwise. p, q contains multipliers
+bool simple_factorization(mpz &n, mpz &p, mpz &q){
+    mpz reminder;
+    unsigned int divider = 2;
+    // Iters 1 000 000 times
+    for (int i = 0; i < ITERATIONS; ++i) {
 
+        mpz_mod_ui(reminder.value, n.value, divider);
+
+        if (mpz_sgn(reminder.value) == 0) {
+            mpz res;
+            mpz mpz_divider;
+
+            mpz_set_ui(mpz_divider.value, divider);
+
+            mpz_div_ui(res.value, n.value, divider);
+
+            if ( miller_rabin_is_prime(res, K) && miller_rabin_is_prime(mpz_divider, K) ){
+
+                mpz_set(p.value, mpz_divider.value);
+                mpz_set(q.value, res.value);
+                return true;
+            }
+        }
+
+        ++divider;
+    }
+
+    mpz_set_ui(p.value, 0);
+    mpz_set_ui(q.value, 0);
+    return false;
+}
+
+
+void pollar_rho_factorization_star(mpz &n, mpz &p, mpz &q){
+
+
+    if (mpz_cmp_ui(n.value, 1) == 0){
+        std:: cout << "n pho_factor" << std::endl;
+        return;
+    }
+
+    mpz two_reminder;
+    mpz_mod_ui(two_reminder.value, n.value, 2);
+    if (mpz_sgn(two_reminder.value) == 0){
+        std::cout << "obe factor is 2" << std::endl;
+        return;
+    }
+
+    mpz x, y, d, c;
+    set_random(2, n.value, x.value);    // Set x <- [2 .. N)
+
+    mpz_set(y.value, x.value);  // Set y <- x
+
+
+    set_random(1, n.value, c.value);    // Set c <- [2 .. N)
+
+    display("c: ", c.value);
+
+    mpz_set_ui(d.value, 1);
+
+    while (mpz_cmp_ui(d.value, 1) == 0){
+
+        mpz_powm_ui(x.value, x.value, 2, n.value);  // x <- x^2 mod n   x <- f(x)
+        mpz aux_x;
+        mpz_add(aux_x.value, c.value, n.value);   // aux_x <- c + n
+        mpz_add(x.value, x.value, aux_x.value);   // x <- x + aux_x ~ (x^2 mod n) + c + n
+        mpz_mod(x.value, x.value, n.value); // x <-  ((x^2 mod n) + c + n) mod n
+
+
+        mpz_powm_ui(x.value, x.value, 2, n.value);
+        mpz_powm_ui(x.value, x.value, 2, n.value);  // y <- f(f(y))
+        mpz aux_y;
+        mpz_add(aux_y.value, c.value, n.value);   // aux_y <- c + n
+        mpz_add(y.value, y.value, aux_y.value);   // y <- y + aux_x ~ (y^2 mod n) + c + n
+        mpz_mod(y.value, y.value, n.value); // x <-  ((y^2 mod n) + c + n) mod n
+
+
+
+
+        mpz substract;
+
+        mpz_sub(substract.value, x.value, y.value);
+        mpz_abs(substract.value, substract.value);  // Calculate |x - y|
+
+        set_gcd(substract.value, n.value, d.value); // Calculate GDC(|x-y|, n)
+
+
+
+        if (mpz_cmp(d.value, n.value) == 0){
+            pollar_rho_factorization_star(n, p, q);
+        }
+    }
+
+
+    mpz_set(p.value, d.value);
+    mpz_div(q.value, n.value, d.value);
+
+}
+
+void RSA_cracker(mpz &n){
+    mpz n_cpy;
+    mpz_set(n_cpy.value,n.value);
+
+
+    mpz p, q;
+    if (simple_factorization(n, p, q)){
+        display("p factorized by simple: ", p.value);
+        display("q factorized by simple: ", q.value);
+
+    } else{
+        pollar_rho_factorization_star(n_cpy, p, q);
+        display("p factorized by rho: ", p.value);
+        display("q factorized by rho: ", q.value);
+    }
+
+}
 
 int main() {
-
     mpz p, q, n, e, d;
 
-    RSA_prepare(p, q, n, e, d, 96);
+    RSA_prepare(p, q, n, e, d, 50);
 
-    std::cout << mpz_sizeinbase(e.value, 2)  <<std::endl;
+//    mpz_set_ui(n.value, 3549910979);
 
-    mpz m, cm;
+    RSA_cracker(n);
+//    std::cout << mpz_sizeinbase(e.value, 2)  <<std::endl;
 
-    mpz_set_ui(m.value, 9234235);
-    display("plain input: ", m.value);
-    cipher(e, n, m, cm);
-
-    display("ciphered text: ", cm.value);
-
-    mpz dm;
-    decipher(d, n, cm, dm);
-
-    display("deciphered: ", dm.value);
+//    mpz m, cm;
+//
+//    std::string input_str = "397a52dccd";
+//
+//    mpz input_val;
+//    mpz_set_ui(input_val.value, 0);
+//
+//    mpz_set_str(input_val.value, input_str.c_str(), 16);
+//
+//
+//    display("plain input: ", input_val.value);
+//
+//    cipher(e, n, input_val, cm);
+//
+//    display("ciphered text: ", cm.value);
+//
+//    mpz dm;
+//    decipher(d, n, cm, dm);
+//
+//    display("deciphered: ", dm.value);
 
 
     return 0;
